2ff8126b587fd39baee86ccc028eb0ec45339e56
Flavio Vicentin <flavicentin10@gmail.com>
2021-11-04 22:46:11 -0300

    sevices

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// open & close menu: hamburguer 
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+
+for (const element of toggle){
+  element.addEventListener('click', function(){
+    nav.classList.toggle('show')
+  })
+}
+
+
+//link menu 
+const links = document.querySelectorAll('nav ul li a');
+
+for (const link of links){
+  link.addEventListener('click', function(){
+    nav.classList.remove('show');
+  })
+}
+
+ 
