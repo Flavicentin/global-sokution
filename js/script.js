737cfb0847b4f70ff55b9d07a1d6603dd5e9f8ea
Flavio Vicentin <flavicentin10@gmail.com>
2021-11-05 21:49:33 -0300

    mobile-ready

--- js/script.js
+++ js/script.js
@@ -18,4 +18,39 @@ for (const link of links){
   })
 }
 
+//Back to top
+const backToTopButton = document.querySelector('.back-to-top');
+window.addEventListener('scroll', function(){
+  if(window.scrollY >= 560){
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+})
+
+//swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination:{
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keybord: true
+});
+
+// scrollreveal
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+});
+
+scrollReveal.reveal('#home',{interval: 100});
+scrollReveal.reveal('#about ',{interval: 100});
+scrollReveal.reveal('#services, #services .cards ',{interval: 100});
+scrollReveal.reveal('#testimonials',{interval: 100});
+scrollReveal.reveal('#contact',{interval: 100});
+scrollReveal.reveal('footer .brand, footer',{interval: 100});
+
  
